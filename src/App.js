6c1909d72e5cf6979b6bdacd0b236e0d220752bc
anas110440 <75642207+anas110440@users.noreply.github.com>
2022-10-19 10:28:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route  path="/" element={hasAccount? <Dashboard setHasAccount={setHasAccount} /> :
+      <Route  path="/dashboard" element={hasAccount? <Dashboard setHasAccount={setHasAccount} /> :
       <Login setHasAccount={setHasAccount} />} />
 
       <Route path='/category' element={hasAccount?  <Categories />  :
